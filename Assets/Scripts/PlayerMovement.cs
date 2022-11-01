42f9209cca4ddbdf8b925e566f29d2b901098078
gleb-gavrilov <y1@bk.ru>
2022-11-02 00:31:34 +0300

    add animation for player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,20 +2,29 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
+    private const string RunAnimation = "IsRun";
+    private const string JumpTrigger = "Jump";
+    private const string GroundedAnimation = "IsGrounded";
+    private const string Ground = "Ground";
+
     private Rigidbody2D _rigidBody;
     private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
     private bool _isRotate;
+    private bool _isGround;
     private float _axisValue;
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -25,10 +34,27 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _speed);
+            Jump();
         }
 
         _isRotate = _axisValue < 0 ? true : false;
         _spriteRenderer.flipX = _isRotate;
+        _animator.SetBool(RunAnimation, _axisValue != 0);
+        _animator.SetBool(GroundedAnimation, _isGround);
+    }
+
+    private void Jump()
+    {
+        _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _speed);
+        _animator.SetTrigger(JumpTrigger);
+        _isGround = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(Ground))
+        {
+            _isGround = true;
+        }
     }
 }
